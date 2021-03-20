12feca5ba6a323e798f7e2d1b53e636ffd184cae
Madeeha-Anjum <maadeeeehaa@gmail.com>
2021-03-20 12:28:11 -0600

    --

--- libbarrett/src/bus/bus_manager.cpp
+++ libbarrett/src/bus/bus_manager.cpp
@@ -39,115 +39,112 @@
 #include <barrett/bus/can_socket.h>
 #include <barrett/bus/bus_manager.h>
 
-
-namespace barrett {
-namespace bus {
-
-
-BusManager::BusManager(CommunicationsBus* _bus) :
-	bus(_bus), deleteBus(false), messageBuffers()
+namespace barrett
 {
-	if (bus == NULL) {
-		bus = new CANSocket;
-		deleteBus = true;
-	}
-}
+	namespace bus
+	{
 
-BusManager::BusManager(int port) :
-	bus(NULL), deleteBus(true), messageBuffers()
-{
-	bus = new CANSocket(port);
-}
-
-BusManager::~BusManager()
-{
-	if (deleteBus) {
-		delete bus;
-	}
-}
-
-int BusManager::receive(int expectedBusId, unsigned char* data, size_t& len, bool blocking, bool realtime) const
-{
-	thread::Mutex& m = getMutex();
-	m.lock();
-
-	double start = highResolutionSystemTime();
-
-	if (retrieveMessage(expectedBusId, data, len)) {
-		m.unlock();
-		return 0;
-	}
-
-	int ret;
-	while (true) {
-		ret = updateBuffers();
-		if (ret != 0) {
-			m.unlock();
-			return ret;
-		} else if (retrieveMessage(expectedBusId, data, len)) {
-			m.unlock();
-			return 0;
-		} else if (!blocking) {
-			m.unlock();
-			return 1;
+		BusManager::BusManager(CommunicationsBus *_bus) : bus(_bus), deleteBus(false), messageBuffers()
+		{
+			// if (bus == NULL) {
+			// 	bus = new CANSocket;
+			// 	deleteBus = true;
+			// }
 		}
 
-		if ((highResolutionSystemTime() - start) > CommunicationsBus::TIMEOUT) {
-			m.unlock();
-			logMessage("BusManager::receive(): timed out", true);
-			return 2;
+		BusManager::BusManager(int port) : bus(NULL), deleteBus(true), messageBuffers()
+		{
+			// bus = new CANSocket(port);
 		}
 
-		if (!realtime) {
-			int lc = m.fullUnlock();
-			btsleepRT(0.0001);
-			m.relock(lc);
+		BusManager::~BusManager()
+		{
+			// if (deleteBus) {
+			// 	delete bus;
+			// }
 		}
-	}
-}
 
-int BusManager::updateBuffers() const
-{
-	BARRETT_SCOPED_LOCK(getMutex());
+		int BusManager::receive(int expectedBusId, unsigned char *data, size_t &len, bool blocking, bool realtime) const
+		{
+			// thread::Mutex& m = getMutex();
+			// m.lock();
 
-	int busId;
-	unsigned char data[CommunicationsBus::MAX_MESSAGE_LEN];
-	size_t len;
-	int ret;
+			// double start = highResolutionSystemTime();
 
-	// empty the bus' receive buffer
-	while (true) {
-		ret = receiveRaw(busId, data, len, false);  // non-blocking read
-		if (ret == 0) {  // successfully received a message
-			if (busId != 1344) storeMessage(busId, data, len); // disregard safetyboard broadcast message
-		} else if (ret == 1) {  // would block
-			return 0;
-		} else {  // error
-			return ret;
+			// if (retrieveMessage(expectedBusId, data, len)) {
+			// 	m.unlock();
+			// 	return 0;
+			// }
+
+			// int ret;
+			// while (true) {
+			// 	ret = updateBuffers();
+			// 	if (ret != 0) {
+			// 		m.unlock();
+			// 		return ret;
+			// 	} else if (retrieveMessage(expectedBusId, data, len)) {
+			// 		m.unlock();
+			// 		return 0;
+			// 	} else if (!blocking) {
+			// 		m.unlock();
+			// 		return 1;
+			// 	}
+
+			// 	if ((highResolutionSystemTime() - start) > CommunicationsBus::TIMEOUT) {
+			// 		m.unlock();
+			// 		logMessage("BusManager::receive(): timed out", true);
+			// 		return 2;
+			// 	}
+
+			// 	if (!realtime) {
+			// 		int lc = m.fullUnlock();
+			// 		btsleepRT(0.0001);
+			// 		m.relock(lc);
+			// 	}
+			// }
 		}
-	}
-}
 
-void BusManager::storeMessage(int busId, const unsigned char* data, size_t len) const
-{
-	if (messageBuffers[busId].full()) {
-		(logMessage("BusManager::%s: Buffer overflow. ID = %d",true) %__func__ %busId).raise<std::runtime_error>();
-	}
-	messageBuffers[busId].push_back(Message(data, len));
-}
+		int BusManager::updateBuffers() const
+		{
+			// BARRETT_SCOPED_LOCK(getMutex());
 
-bool BusManager::retrieveMessage(int busId, unsigned char* data, size_t& len) const
-{
-	if (messageBuffers[busId].empty()) {
-		return false;
-	}
+			// int busId;
+			// unsigned char data[CommunicationsBus::MAX_MESSAGE_LEN];
+			// size_t len;
+			// int ret;
 
-	messageBuffers[busId].front().copyTo(data, len);
-	messageBuffers[busId].pop_front();
+			// // empty the bus' receive buffer
+			// while (true) {
+			// 	ret = receiveRaw(busId, data, len, false);  // non-blocking read
+			// 	if (ret == 0) {  // successfully received a message
+			// 		if (busId != 1344) storeMessage(busId, data, len); // disregard safetyboard broadcast message
+			// 	} else if (ret == 1) {  // would block
+			// 		return 0;
+			// 	} else {  // error
+			// 		return ret;
+			// 	}
+			// }
+		}
 
-	return true;
-}
+		void BusManager::storeMessage(int busId, const unsigned char *data, size_t len) const
+		{
+			// if (messageBuffers[busId].full()) {
+			// 	(logMessage("BusManager::%s: Buffer overflow. ID = %d",true) %__func__ %busId).raise<std::runtime_error>();
+			// }
+			// messageBuffers[busId].push_back(Message(data, len));
+		}
 
+		bool BusManager::retrieveMessage(int busId, unsigned char *data, size_t &len) const
+		{
+			// if (messageBuffers[busId].empty()) {
+			// 	return false;
+			// }
 
-}
-}
+			// messageBuffers[busId].front().copyTo(data, len);
+			// messageBuffers[busId].pop_front();
+
+			// return true;
+		}
+
+	} // namespace bus
+} // namespace barrett
